ba3cabcf348f898d20965eb4930185907dac2887
SamreenKausar <kausarsamreen28@gmail.com>
2023-05-24 13:32:03 +0400

    Restructured the modules

--- app.js
+++ app.js
@@ -1,180 +1,79 @@
-// GAME BOARD MODULE
-const gameBoard = (function gameBoard() {
-  const gameBoardArray = new Array(9);
+// Game Board Module
 
+const gameBoardModule = (function gameBoardModule() {
+  const gameboard = new Array(9);
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
   ];
-  const addMark = (index, mark) => {
-    gameBoardArray[index] = mark;
+  const addMarksinArray = (index, mark) => {
+    if (gameboard[index] === undefined) {
+      gameboard[index] = mark;
+    }
   };
-  const checkForWin = (mark) => winningCombinations.some((combination) => combination.every((index) => gameBoardArray[index] === mark));
-
-  return { addMark, checkForWin };
+  const checkForWin = (mark) => winningCombinations.some(
+    (combination) => combination.every(
+      (index) => gameboard[index] === mark,
+    ),
+  );
+  return { gameboard, addMarksinArray, checkForWin };
 }());
 
-// PLAYER FACTORY FUNCTION
-const players = (mark) => {
-  let score = 0;
+// Dom Manupulation Module
 
-  const increaseScore = (win) => {
-    if (win) {
-      score += 1;
-    }
-
-    const returnscore = () => score;
-    return { mark, returnscore, increaseScore };
-  };
-};
-// DOM OBJECT MODULE
-const domManupulation = (function domManupulation() {
+const displayGameboard = (function displayGameboard() {
   const cells = document.querySelectorAll('.cell');
 
-  const getCellIndex = function getCellIndex() {
+  const paintCell = () => {
     cells.forEach((cell) => {
-      cell.addEventListener('click', (e) => {
-        const index = e.target.dataset.number;
-      });
+      cell.textContent = gameBoardModule.gameboard[cell.dataset.number];
+      if (gameBoardModule.gameboard[cell.dataset.number] === 'x') {
+        cell.classList.add('colorX');
+      } else {
+        cell.classList.add('colorO');
+      }
     });
   };
-
-  const paintCellsWithMark = (index, mark) => {
-    cells[index].textContent = mark;
-  };
-  return { getCellIndex };
+  return { paintCell };
 }());
 
-// GAME CONTROL MODULE
+// player factory function
 
-const gameBoardController = (function gameBoardController() {
-  const startGamebtn = document.querySelector('#startGame');
-  const playfuntion = function playfuntion() {
-
+const createPlayer = (name, mark) => {
+  let score = 0;
+  const increaseScore = () => {
+    score += 1;
   };
+  return { name, mark, increaseScore };
+};
 
-  startGamebtn.addEventListener('click', playfuntion);
+// game Control Module
+
+const gameControl = (function gameControl() {
+  const circleTurn = false;
+  const win = false;
+  const draw = false;
+  const cells = document.querySelectorAll('.cell');
+  const startGame = function startGame() {
+    cells.forEach((cell) => {
+      cell.addEventListener('click', handleClick, { once: true });
+    });
+  };
+  const handleClick = function handleClick(e) {
+    const index = e.target.number;
+    console.log(index);
+    const mark = circleTurn ? 'O' : 'X';
+    gameBoardModule.addMarksinArray(index, mark);
+    displayGameboard.paintCell();
+  };
+  return{startGame}
 }());
 
-// GAME BOARD OBJECT
-// const gameBoard = (function(){
-//     const gameboard =[];
-//     const addingValue = function(index, value){
-
-//     }
-//     return {
-//         addingValue
-//     }
-// });
-
-// // PLAYERS FACTORY FUNCTION
-
-// const players = function(playerName){
-//     let score =0;
-//     let win =false;
-//     const changeScore = (win)=>
-//     {
-//         if(!win){
-//             score++;
-//         }
-//     };
-
-//     return{ playerName, score, win, changeScore};
-
-// }
-
-// // DISPLAY CONTROLLER
-
-// // game Board module
-// const gameBoardController = (function(){
-//     //properties
-//     const cells = document.querySelectorAll('.cell');
-//     const winMessage =document.querySelector('.winner');
-//     const winElement =document.querySelector("#win");
-//     const X = 'X';
-//     const O = 'O';
-//     const xColor = 'colorX';
-//     const oColor = 'colorO';
-//     const drawColor ='drawColor';
-//     const winningCombinations = [
-//         [0,1,2],
-//         [3,4,5],
-//         [6,7,8],
-//         [0,3,6],
-//         [1,4,7],
-//         [2,5,8],
-//         [0,4,8],
-//         [2,4,6]
-//     ]
-//     let circleTurn = false;
-
-//     cellArray = [...cells];
-//     //Methods
-//      const startGame = function(){
-//         cells.forEach(cell =>{
-//             cell.addEventListener('click', handleClick, {once: true});
-//         });
-//      }
-
-//      const handleClick = function(e){
-//         const cell= e.target;
-//         const currentText = circleTurn ? O : X;
-//         const textColor = circleTurn ? oColor : xColor;
-//         // place a Mark
-//         placeMark(cell, currentText, textColor);
-//         // check for win
-//         if(checkForWin(currentText)){
-//             endGame(false, textColor, currentText);
-//          }
-//           // check for draw
-//          if(isDraw()){
-//             endGame(true, drawColor, 'Its a draw');
-
-//          }else{
-//             // switch turns
-//             swapTurn();
-//         }
-
-//      }
-
-//      const placeMark = function(cell, text, textColor){
-//         cell.textContent = text;
-//         cell.classList.add(textColor)
-
-//      }
-//      const swapTurn = function(){
-//         circleTurn=!circleTurn;
-//      }
-//     const checkForWin = function(text){
-//         return winningCombinations.some(combination =>{
-//             return combination.every(index =>{
-//                return cellArray[index].textContent===text;
-//             })
-//         })
-//     }
-//     const endGame = function(draw, textColor, currentText){
-//         if(draw){
-//             winMessage.classList.toggle('hidden');
-//             winElement.textContent = currentText;
-//             winMessage.classList.add(textColor);
-//         }else {
-//             winMessage.classList.toggle('hidden');
-//             winElement.textContent = `${currentText} Wins`;
-//             winMessage.classList.add(textColor);
-
-//         }
-//     }
-//     const isDraw = function(){
-//         return [...cells].every(element =>{
-//             return element.classList.contains(xColor) || element.classList.contains(oColor);
-//         });
-//     }
-
-//     return{startGame}
-
-// })();
-// const startGame  = document.querySelector('#startGame');
-//  startGame.addEventListener('click',gameBoardController.startGame);
+const startBtn = document.querySelector('#startGame');
+startBtn.addEventListener('click', gameControl.startGame);
